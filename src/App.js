acf0903220351b34bf30cf2a19fcdb91b2a5d895
simmo75 <102128512+simmo75@users.noreply.github.com>
2022-06-06 14:35:39 -0700

    added-answer-component-for-question

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import Answer from "./components/Answer";
 
-// function App() {
-//   const [answers, setAnswers] = useState([
-//     {
-//       pk: 1,
-//       description: "I think its a cardinal, can anyone confirm?",
-//       author: "Sammy Davis Junior",
-//       created_at: "June 1, 2022",
-//     },
-//     {
-//       pk: 1,
-//       description: "I must know.",
-//       author: "Frank Sinatra",
-//       created_at: "June 2, 2022",
-//     },
-//   ]);
-
-//   return (
-//     <>
-//       {answers.map((answer) => (
-//         <Answer
-//           description={answer.description}
-//           created_at={answer.created_at}
-//         />
-//       ))}
-//     </>
-//   );
-// }
+function App() {
+  return <Answer />;
+}
 
 export default App;
