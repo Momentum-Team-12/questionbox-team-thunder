44723f708f0f0a835702304e37d03f7cc24375d5
hjoyheath <hillaryjoyheath@gmail.com>
2022-06-06 21:57:21 -0400

    added register link

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import { useRef } from 'react';
+import { Link } from 'react-router-dom';
 import classes from './Login.module.css';
 
 function Login (props) {
@@ -31,7 +32,12 @@ return(
          <div className={classes.actions}>
              <button>Submit</button>
          </div>
-     </form>
+     <ul>
+        <li>
+        <Link to='/register'>Register</Link>
+        </li>
+    </ul>
+    </form>
     )
 }
 
