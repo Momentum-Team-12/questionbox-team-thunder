1d98e81a8181db0b83515d2d6b7112c3fd88a4a0
hjoyheath <hillaryjoyheath@gmail.com>
2022-06-08 11:21:57 -0400

    getting ready to merge

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,37 +1,44 @@
-import { useRef } from 'react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import axios from 'axios';
 import classes from './Login.module.css';
 
-function Login (props) {
-    const usernameInputRef = useRef();
-    const passwordInputRef = useRef();
+function Login ({ setAuth, isLoggedIn }) {
+   const [username, setUsername] = useState('')
+   const [password, setPassword] = useState('')
+   const [error, setError] = useState('')
 
-    function submitHandler(event){
-        event.preventDefault();
+    const handleLogin = (event) => {
+        event.preventDefault()
+        setError('')
 
-        const enteredUsername = usernameInputRef.current.value;
-        const enteredPassword = passwordInputRef.current.value;
-
-        const loginData= {
-            username: enteredUsername,
-            password: enteredPassword,
+        axios.post('https://questionbox-team-thunder-api.herokuapp.com/api/auth/token/login/', {
+            username: username,
+            password: password,
+        })
+        .then((res) => {
+            setAuth(username, res.data.auth_token)
         }
-        props.onAddLogin(loginData); 
+        )
+        .catch((event) => setError(event.message))
+    }
+    if (isLoggedIn) {
+        return <Navigate to="/" replace={true} />
     }
 
 return(
-        <form onSubmit={submitHandler}>
+        <form onSubmit={handleLogin}>
         <h2>Login</h2>
          <div className={classes.control}>
              <label htmlFor="username">Username </label>
-             <input type="text" required id="username" ref={usernameInputRef} />
+             <input type="text" required id="username" onChange={(event) => setUsername(event.target.value)} />
          </div>
          <div className={classes.control}>
              <label htmlFor="password">Password </label>
-             <input type="password" required id="password" ref={passwordInputRef} />
+             <input type="password" required id="password" onChange={(event) => setPassword(event.target.value)} />
          </div>
          <div className={classes.actions}>
-             <button>Submit</button>
+             <button>Login</button>
          </div>
      <ul>
         <li>
