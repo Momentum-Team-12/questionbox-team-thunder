9236be4b01ec8755b26bf61f6bd70f1b226087b8
hjoyheath <hillaryjoyheath@gmail.com>
2022-06-09 17:35:46 -0400

    prep to merge

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/ask" element={<Ask isLoggedIn={isLoggedIn} />} />
+        <Route path="/ask" element={<Ask isLoggedIn={isLoggedIn} token={token} />} />
         <Route
           path="/login"
           element={<Login setAuth={setAuth} isLoggedIn={isLoggedIn} />}
